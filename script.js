3a8dbae9b5e11f24a6b8c33c96db191427463874
Yuxuan <nine.slide@gmail.com>
2022-09-13 16:48:50 +0200

    scroll

--- script.js
+++ script.js
@@ -8,24 +8,53 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+const openModal = function (e) {
+    e.preventDefault();
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
 };
 
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach((el) => {
+    return el.addEventListener('click', openModal);
+});
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  }
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});
+
+//** learn more scrolling */
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', (_e) => {
+    const s1coords = section1.getBoundingClientRect();
+    // console.log('current scroll', window.pagexoffset, window.pageyoffset);
+    // console.log('height/width vp:' ,docuemnt.docuemntElement.clientHeight, document.docuemntElement.clientWidth);
+
+    //*? not smooth */
+    // window.scrollTo(
+    //     s1coords.left + window.pageXOffset,
+    //     s1coords.top + window.pageYOffset,
+    // );
+    //**? adding smooth behavior */
+    // window.scrollTo({
+    //     left: s1coords.left + window.pageXOffset,
+    //     top: s1coords.top + window.pageYOffset,
+    //     behavior: 'smooth',
+    // });
+
+
+    //**? mordern JS */
+    section1.scrollIntoView({behavior:'smooth'});
 });
